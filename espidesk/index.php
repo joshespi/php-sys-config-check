c146db835b80fb6965bff2d59d67287247fa5d94
Espi <joshespi@gmail.com>
2022-05-21 19:12:31 -0600

    added simple session check login form

--- espidesk/index.php
+++ espidesk/index.php
@@ -5,19 +5,50 @@
     } else {
         header( 'Location: install.php' );
     }
-//ob_start();
     
+    // Initialize the session
+    session_start();
+
+    // Check if the user is already logged in, if yes then redirect him to welcome page
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+        header("location: dashboard.php");
+        exit;
+    }   
+    
+    // Define variables and initialize with empty values
+    $username = $password = "";
+    $username_err = $password_err = $login_err = "";
+
+    //database information from the DB config file
     $dbhost = $conf['dbhost'];
     $dbuser = $conf['dbuser'];
     $dbpass = $conf['dbpass'];
-    $dbname = 'espidesk';
+    $dbname = $conf['dbname'];
     $link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-    
     if(!$link){
-        
         die("Failed to establish connection");
     } else {
-        echo "Connection established successfully"; 
+    ?>
+      <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>EspiDesk Login</title>
+            <link rel="stylesheet" href="assets/css/variable.css?v=1.0">
+            <link rel="stylesheet" href="assets/css/general.css?v=1.0">
+            <link rel="stylesheet" href="assets/css/installer.css?v=1.0">
+        </head>
+        <body>
+            <h1>Espi Desk Login</h1>
+            <form action="" method="post">
+            <label for="user_name">Username:</label><input type="text" name="user_name" id="user_name" placeholder="Username">
+            <label for="user_pass">Password:</label><input type="password" name="user_pass" id="user_pass" placeholder="Password">
+            <input type="submit" value="Login">
+            </form>
+        </body>
+        </html>
+    <?php
     }
-//ob_end_flush();
+
 ?>
