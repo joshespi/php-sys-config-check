0ccdf9824bbf109f305f1cc7a82414f9a1b1732e
Espi <joshespi@gmail.com>
2022-05-26 08:45:02 -0600

    created global footer file

---
+++ espidesk/footer.php
@@ -0,0 +1,12 @@
+</body>
+            </html>
+
+
+<h1>session</h1>
+<?php print_r($_SESSION); ?>
+<h1>post</h1>
+<?php print_r($_POST); ?>
+<h1>request</h1>
+<?php print_r($_REQUEST); ?>
+<h1>Files</h1>
+<?php print_r($_FILES); ?>
