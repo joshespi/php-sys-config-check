3b46edf6e7bb58945a987fa614f7f3080577218c
Espi <joshespi@gmail.com>
2022-05-26 08:45:08 -0600

    created Global header file

---
+++ espidesk/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+            <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>help.provo.edu</title>
+                <link rel="stylesheet" href="assets/css/variable.css?v=1.0">
+                <link rel="stylesheet" href="assets/css/general.css?v=1.0">
+                <link rel="stylesheet" href="assets/css/installer.css?v=1.0">
+            </head>
+            <body>
